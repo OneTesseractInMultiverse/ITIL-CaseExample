b8b3c221882800111a35698240a6d60ee0b98d70
Pedro Guzman <pguzmanb498@ulacit.ed.cr>
2017-11-24 03:23:15 +0000

    Case added

--- courseware/controllers/service_controller.py
+++ courseware/controllers/service_controller.py
@@ -8,3 +8,21 @@ from flask import render_template
 @app.route('/service/1', methods=['GET'])
 def service_1():
     return render_template("home/auth_service.html")
+
+
+# --------------------------------------------------------------------------
+# GET /
+# --------------------------------------------------------------------------
+# Root resource
+@app.route('/service/2', methods=['GET'])
+def service_2():
+    return render_template("home/consulting_service.html")
+    
+
+# --------------------------------------------------------------------------
+# GET /
+# --------------------------------------------------------------------------
+# Root resource
+@app.route('/service/3', methods=['GET'])
+def service_3():
+    return render_template("home/hosting_service.html")
